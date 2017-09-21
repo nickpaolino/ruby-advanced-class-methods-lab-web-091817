02a09d9e498315d6efecb205e22f7bd678b7370b
github username <github email address>
2017-09-21 17:44:26 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,47 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = Song.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|current_song| current_song.name == song_name} # Can use .detect instead of .find
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = Song.new
+    song.artist_name, song.name = filename.split(/ - |.mp3/)
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
